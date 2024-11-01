a6705a2bae50c84c72090b0eb21f5c3263100ca2
Livia-1212 <62072577+Livia-1212@users.noreply.github.com>
2024-11-01 13:55:45 -0400

    updated Day 4 -Rock Paper Scissors

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -29,12 +29,14 @@ scissors = '''
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, and 2 for Scissors: \n "))
 user_image = ([rock, paper, scissors])
-print(f"Your choice:{user_image[user_choice]}")
+try:
+    print(f"Your choice:{user_image[user_choice]}")
+except IndexError:
+    print("You typed an invalid number. Try again!")
 
 computer_choice = random.randint(0,2)
 computer = [rock, paper, scissors]
 print(f"Computer choice: {computer[computer_choice]}")
-
 
 if user_choice == computer_choice:
     print("It's a tie.")
@@ -46,5 +48,7 @@ elif user_choice == 2 and computer_choice == 1:
     print("You win!")
 else:
     print("You lose!")
+
+
 
 
