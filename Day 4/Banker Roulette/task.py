5475c90c650960c0a305d14d5083cf506395f276
Livia-1212 <62072577+Livia-1212@users.noreply.github.com>
2025-05-19 11:22:03 -0400

    updated projects

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,5 +1,1 @@
-import random
-
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
-
-print(f'{random.choice(friends)}')
--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,5 +1,1 @@
 fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " pie")
-
