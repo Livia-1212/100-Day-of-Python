11ccaf3fb0a0fbdc2bce4d2dbd51482ec6426ed3
Livia-1212 <62072577+Livia-1212@users.noreply.github.com>
2025-06-11 15:46:09 -0400

    Day 9 completed

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -5,13 +5,13 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# def encrypt(original_text, shift_amount):
-#     cipher_text = ""
-#     for letter in original_text:
-#         shifted_position = alphabet.index(letter) + shift_amount
-#         shifted_position %= len(alphabet)
-#         cipher_text += alphabet[shifted_position]
-#     print(f"Here is the encoded result: {cipher_text}")
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+    print(f"Here is the encoded result: {cipher_text}")
 
 
 
@@ -22,14 +22,14 @@ shift = int(input("Type the shift number:\n"))
 # TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
 # TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
 #  by the shift amount and print the decrypted text.
-# def decrypt(original_text, shift_amount):
-#     decode_text = ""
-#     for letter in original_text:
-#         shift_amount *= -1
-#         reverse_position = alphabet.index(letter) - shift_amount
-#         reverse_position %= len(alphabet)
-#         decode_text += alphabet[reverse_position]
-#     print(f"Here is the decoded result: {decode_text}")
+def decrypt(original_text, shift_amount):
+    decode_text = ""
+    for letter in original_text:
+        shift_amount *= -1
+        reverse_position = alphabet.index(letter) - shift_amount
+        reverse_position %= len(alphabet)
+        decode_text += alphabet[reverse_position]
+    print(f"Here is the decoded result: {decode_text}")
 
 
 
--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,38 @@
 # TODO-1: Ask the user for input
+import art
+print(art.logo)
+
+name = ""
+price = 0
+bidding_dictionary = {}
+
 # TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
+# biding_dictionary[name] = price
+
+
 # TODO-4: Compare bids in dictionary
+
+def find_highest_bidder(bidding_dictionary):
+    winner = max(bidding_dictionary, key=bidding_dictionary.get)
+
+    highest_bid = bidding_dictionary[winner]
+
+    print(f"The winner is {winner} with a bid amount of ${highest_bid}.")
+
+# TODO-3: Whether if new bids need to be added
+
+# question = input("Are there any other bidders? Type 'yes' or 'no'. \n")
+
+continue_bidding = True
+while continue_bidding:
+    name = input("Enter your name if you are going to bid: ")
+    price = int(input("What is the price that you are bidding: $"))
+    bidding_dictionary[name] = price
+    question = input("Are there any other bidders? Type 'yes' or 'no'. \n").lower()
+    if question == 'no':
+        continue_bidding = False
+        find_highest_bidder(bidding_dictionary)
+    elif question == 'yes':
+        print("\n" * 20)
 
 
